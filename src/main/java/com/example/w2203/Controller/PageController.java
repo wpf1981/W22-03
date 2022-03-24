c4ef8518aab8fa7a03fd2bb4e5a0587f411baca3
wpf1981 <45836699+wpf1981@users.noreply.github.com>
2022-03-24 17:10:14 +0800

    02

---
+++ src/main/java/com/example/w2203/Controller/PageController.java
@@ -0,0 +1,18 @@
+package com.example.w2203.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class PageController {
+    //登录页面，未设置
+
+    @GetMapping("/i")
+    public String login() {
+        return "index";
+    }
+}
