e58fa63647c2f8fa82d5dff6c7d82e12fa8fed80
wpf1981 <45836699+wpf1981@users.noreply.github.com>
2022-03-24 16:16:18 +0800

    01

---
+++ src/main/java/com/example/w2203/W2203Application.java
@@ -0,0 +1,13 @@
+package com.example.w2203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2203Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W2203Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/w2203/W2203ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w2203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2203ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
