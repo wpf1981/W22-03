e76419948dc932283eed8082ebf0b80fc0380bab
wpf1981 <45836699+wpf1981@users.noreply.github.com>
2022-03-28 10:57:27 +0800

    05-Swagger2

---
+++ src/main/java/com/example/w2203/Config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.example.w2203.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Profiles;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket serviceDocket()    {
+//        Environment env
+        //设置要暴漏接口文档的配置环境
+        Profiles profile = Profiles.of("dev", "test");
+
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+//                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger3-Demo接口文档")
+                .description("技术支持")
+//                .contact(new Contact("云嘉健康技术团队", "http://www.yunjiacloud.com", "duchong@yunjiacloud.com "))
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/com/example/w2203/Controller/PageController.java
+++ src/main/java/com/example/w2203/Controller/PageController.java
@@ -2,6 +2,8 @@ package com.example.w2203.Controller;
 
 import com.example.w2203.Entity.TestTab;
 import com.example.w2203.Repository.TestRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,17 +13,19 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/")
+@Api(tags = "测试用接口")
 public class PageController {
     @Resource
     TestRepository testRepository;
     //登录页面，未设置
     @GetMapping("/list")
+    @ApiOperation("获取所有数据")
     public List<TestTab> findUserList(){
         return testRepository.findAll();
     }
 
     @PostMapping(value = "/user")
-
+    @ApiOperation("更新指定数据")
     public TestTab addUser(@RequestParam("id") String userid, @RequestParam("name") String username){
 
         TestTab user = new TestTab();
