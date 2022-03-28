0090b724489bea04f788f5f441e1381524b77a11
wpf1981 <45836699+wpf1981@users.noreply.github.com>
2022-03-28 09:57:06 +0800

    05-Swagger2

--- src/main/java/com/example/w2203/W2203Application.java
+++ src/main/java/com/example/w2203/W2203Application.java
@@ -2,6 +2,10 @@ package com.example.w2203;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
@@ -11,5 +15,16 @@ public class W2203Application {
     public static void main(String[] args) {
         SpringApplication.run(W2203Application.class, args);
     }
+
+    @Bean
+    public Docket serviceDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example"))
+//                .paths(PathSelectors.ant("/"))
+                .build();
+    }
+
 
 }
