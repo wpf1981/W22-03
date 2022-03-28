ee9aa2764af40e6d7289b8a0380d67115eb9a685
wpf1981 <45836699+wpf1981@users.noreply.github.com>
2022-03-28 11:39:41 +0800

    06-Swagger3

--- src/main/java/com/example/w2203/Controller/PageController.java
+++ src/main/java/com/example/w2203/Controller/PageController.java
@@ -3,6 +3,8 @@ package com.example.w2203.Controller;
 import com.example.w2203.Entity.TestTab;
 import com.example.w2203.Repository.TestRepository;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +21,19 @@ public class PageController {
     TestRepository testRepository;
     //登录页面，未设置
     @GetMapping("/list")
+
     @ApiOperation("获取所有数据")
     public List<TestTab> findUserList(){
         return testRepository.findAll();
     }
 
     @PostMapping(value = "/user")
+    @ApiImplicitParams(
+            {
+                    @ApiImplicitParam(name = "id",value = "编号",required = true,paramType = "query"),
+                    @ApiImplicitParam(name = "name",value = "名称",required = true,paramType = "query")
+            }
+    )
     @ApiOperation("更新指定数据")
     public TestTab addUser(@RequestParam("id") String userid, @RequestParam("name") String username){
 
