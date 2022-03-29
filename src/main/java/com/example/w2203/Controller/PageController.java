778bf280d82f963d815fa31e7c0f6b3d537b33d2
wpf1981 <45836699+wpf1981@users.noreply.github.com>
2022-03-29 10:49:27 +0800

    08-Swagger3

--- src/main/java/com/example/w2203/Controller/PageController.java
+++ src/main/java/com/example/w2203/Controller/PageController.java
@@ -11,8 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -21,39 +19,57 @@ import java.util.Optional;
 public class PageController {
     @Resource
     TestRepository testRepository;
-    //登录页面，未设置
+    //
     @GetMapping("/tt")
     @ApiOperation("获取所有数据")
-    public List<TestTab> findUserList(){
-        return testRepository.findAll();
+//    public List<TestTab> findUserList(){
+//        return testRepository.findAll();
+//    }
+    public Response findUserList(){
+        return Response.success(testRepository.findAll(),"还可以吧！");
     }
 
     @GetMapping("/tt/{id}")
     @ApiOperation("获取指定数据")
-    public Optional<TestTab> findUser(@PathVariable("id") String userid){
-        log.info(userid);
-        return testRepository.findById(userid);
-    }
+//    public Optional<TestTab> findUser(@PathVariable("id") String userid){
+//        log.info(userid);
+//        return testRepository.findById(userid);
+//    }
 
+    public Response findUser(@PathVariable("id") String userid){
+        log.info(userid);
+        return Response.success(testRepository.findById(userid),"很是不错奥！");
+    }
     @PostMapping(value = "/tt")
+    @ApiOperation("添加新数据")
     @ApiImplicitParams(
             {@ApiImplicitParam(name = "id",value = "编号",required = true,paramType = "query"),
              @ApiImplicitParam(name = "name",value = "名称",required = true,paramType = "query")
             }    )
-    @ApiOperation("添加新数据")
-    public TestTab addUser(@RequestParam("id") String userid,
+//    public TestTab addUser(@RequestParam("id") String userid,
+//                           @RequestParam("name") String username){
+//        TestTab user = new TestTab();
+//        user.setUserId(userid);
+//        user.setUserName(username);
+//        return testRepository.save(user);
+//    }
+    public Response addUser(@RequestParam("id") String userid,
                            @RequestParam("name") String username){
         TestTab user = new TestTab();
         user.setUserId(userid);
         user.setUserName(username);
-        return testRepository.save(user);
+        return Response.success(testRepository.save(user),"新鲜的血液永远最好！");
     }
 
     @PutMapping("/tt")
     @ApiOperation("更新指定数据")
-    public TestTab updateUser(@RequestBody TestTab testTab){
+//    public TestTab updateUser(@RequestBody TestTab testTab){
+//        log.info("ok:"+testTab);
+//        return testRepository.save(testTab);
+//    }
+    public Response updateUser(@RequestBody TestTab testTab){
         log.info("ok:"+testTab);
-        return testRepository.save(testTab);
+        return Response.success(testRepository.save(testTab),"数据无价！");
     }
 
     @DeleteMapping("/tt/{id}")
